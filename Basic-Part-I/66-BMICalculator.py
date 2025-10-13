5caec768b008dabf5383942bad3d4c6853d9da9b
julien palleau <jpalleau@gmail.com>
2025-10-13 18:46:42 +0200

    67-PressureUnitConverter.py

--- Basic-Part-I/66-BMICalculator.py
+++ Basic-Part-I/66-BMICalculator.py
@@ -2,3 +2,10 @@
 """
 Write a Python program to calculate the body mass index.
 """
+import pyinputplus as pyip
+def bmi():
+    weight = pyip.inputFloat('Veuillez saisir votre poid: ')
+    height = pyip.inputFloat('Veuillez saisir votre taille: ')
+    return weight/height**2
+
+print(f"Your body mass index is: {bmi()}")
---
+++ Basic-Part-I/67-PressureUnitConverter.py
@@ -0,0 +1,4 @@
+# https://www.w3resource.com/python-exercises/python-basic-exercises.php
+"""
+Write a Python program to convert pressure in kilopascals to pounds per square inch, a millimeter of mercury (mmHg) and atmosphere pressure.
+"""
